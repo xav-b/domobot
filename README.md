d4aa62bac97aafdb60db7c7dd627dcc8848ea6d8
hackliff <xavier.bruhiere@gmail.com>
2013-11-15 17:46:17 +0100

    first clean before rerunning the project

---
+++ README.md
@@ -0,0 +1,9 @@
+Domobot
+=======
+
+> Home made smart
+
+This repository is currently a place to experiment on domotic projects.
+Main subjects studied at this time are vocal processing (recognition and
+synthetisation), automatic lights controller and image processing (movement
+recognition).
--- deprecated/Processing/libraries/Scene/src/compile.bat
+++
@@ -1,4 +0,0 @@
-javac -O -source 1.5 -target 1.5 -cp libTUIO.jar:core.jar scene\tuio\*.java
-jar cfm ..\library\SceneTuio.jar manifest.inc scene\tuio\SceneTuioBlob.class scene\tuio\SceneTuioListener.class scene\tuio\SceneTuioClient.class scene\tuio\SceneBlob.class
-jar cfm ..\library\Scene.jar manifest.inc scene\tuio\Scene.class
-del scene\tuio\*.class
